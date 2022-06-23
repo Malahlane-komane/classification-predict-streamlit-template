4b38540c1c7f8c78e386c5a2c9777464a5c8c534
Malahlane-komane <101342539+Malahlane-komane@users.noreply.github.com>
2022-06-23 07:57:59 +0200

    new

--- base_app.py
+++ base_app.py
@@ -77,7 +77,11 @@ def main():
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
+			
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
+	
+#creating a contact
+st.header(":mailbox: Get In Touch With Me!")
